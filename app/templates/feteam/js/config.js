60a2bb9796870655aff3397fb37423127039f083
blueflylin <gzlinyi@163.com>
2014-03-27 10:54:38 +0800

    seajs
    seajs

--- app/templates/feteam/js/config.js
+++ app/templates/feteam/js/config.js
@@ -1,11 +1,6 @@
 seajs.config({
     alias: {
-        moment: 'gallery/moment/2.3.1/'
     }
-    // paths: {
-    //     utils: '/js/example/utils',
-    //     static: '/js/example/static',
-    //     logic: '/js/example/logic',
-    //     templates: '/js/example/templates'
-    // }
+    paths: {
+    }
 })
